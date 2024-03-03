8cc4b3758cc8cc72e0c445fc1d52416142b62ba9
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2024-03-04 01:19:01 +0200

    Update cart.js

--- models/cart.js
+++ models/cart.js
@@ -19,7 +19,7 @@ const cartSchema = new Schema(
             required: true,
           },
           imgURL: {
-            type: Number,
+            type: String,
             required: true,
           },
         },
